cd0bec6c90fe27deedd142f7c2e696faa1289a86
seven7739 <josuebalarezo37@gmail.com>
2026-07-14 11:12:12 -0500

    calculadoraEjercicio4

--- header/proyectoFinal.h
+++ header/proyectoFinal.h
@@ -3,7 +3,17 @@
 #define PROYECTOFINAL_H
 #include <string>
 
+#define PI 3.14159265358979323846
+#define ITERACIONES_TRIG 20  
+#define ITERACIONES_LOG 50
+
 bool resolverLaberinto(int arr[][7], int tamFila, int tamCol, int fila, int columna);
+
+double reducir_rango(double x);
+double mi_seno(double x);
+double mi_coseno(double x);
+double mi_ln(double x);
+double mi_tangente(double x);
 
 
 #endif
--- source/proyectoFinal.cpp
+++ source/proyectoFinal.cpp
@@ -1,4 +1,4 @@
-#include "proyectoFinal.h"
+#include "../header/proyectoFinal.h"
 #include <stdio.h>
 
 
@@ -34,3 +34,83 @@ bool resolverLaberinto(int arr[][7], int tamFila, int tamCol, int fila, int colu
     return false;
 
 }
+
+
+
+ 
+
+double reducir_rango(double x) 
+{
+    while (x > PI) 
+	{
+        x -= 2.0 * PI;
+    }
+    while (x < -PI) 
+	{
+        x += 2.0 * PI;
+    }
+    return x;
+}
+
+double mi_seno(double x) 
+{
+    x = reducir_rango(x);
+    double termino = x;
+    double suma = termino;
+    
+    for (int n = 1; n < ITERACIONES_TRIG; n++) 
+	{
+        termino = -termino * (x * x) / ((2 * n) * (2 * n + 1));
+        suma += termino;
+    }
+    return suma;
+}
+
+double mi_coseno(double x)
+{
+    x = reducir_rango(x);
+    double termino = 1.0;
+    double suma = termino;
+    
+    for (int n = 1; n < ITERACIONES_TRIG; n++) {
+        termino = -termino * (x * x) / ((2 * n - 1) * (2 * n));
+        suma += termino;
+    }
+    return suma;
+}
+
+double mi_tangente(double x) 
+{
+    double cos_x = mi_coseno(x);
+    if (cos_x > -0.000000001 && cos_x < 0.000000001) {
+        printf("Error: Tangente indefinida (división por cero).\n");
+        return 0.0; 
+    }
+    return mi_seno(x) / cos_x;
+}
+
+double mi_ln(double x) 
+{
+    if (x <= 0.0) 
+	{
+        printf("Error: El logaritmo solo está definido para números positivos (> 0).\n");
+        return 0.0;
+    }
+    
+    double y = (x - 1.0) / (x + 1.0);
+    double y2 = y * y;
+    double termino = y;
+    double suma = termino;
+    
+    for (int n = 1; n < ITERACIONES_LOG; n++) 
+	{
+        termino *= y2;
+        suma += termino / (2 * n + 1);
+    }
+    
+    return 2.0 * suma;
+}
+
+
+
+
