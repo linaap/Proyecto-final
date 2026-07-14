56f66fcf31178d2b3789faef84e30dcf5472a762
seven7739 <josuebalarezo0@gmail.com>
2026-07-14 11:38:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 int main(){
 	// double anguloRad = PI / 6.0; 
- //    double valorLog = 2.718281828;
+ //    double e = 2.718281828;
 
 
  //    printf("Seno(PI/6)     = %.6f (Esperado: 0.500000)\n", miSeno(anguloRad));
  //    printf("Coseno(PI/6)   = %.6f (Esperado: 0.866025)\n", miCoseno(anguloRad));
  //    printf("Tangente(PI/6) = %.6f (Esperado: 0.577350)\n\n", miTangente(anguloRad));
 
- //    printf("ln(e)          = %.6f (Esperado: 1.000000)\n", miLn(valor_log));
+ //    printf("ln(e)          = %.6f (Esperado: 1.000000)\n", miLn(e));
  //    printf("ln(10)         = %.6f (Esperado: 2.302585)\n", miLn(10.0));
  //    printf("ln(0.5)        = %.6f (Esperado: -0.693147)\n\n", miLn(0.5));
 
