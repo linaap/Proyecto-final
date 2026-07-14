acd18e6a77fd51ab32310bd9f220147cf3be339c
seven7739 <josuebalarezo0@gmail.com>
2026-07-14 11:25:51 -0500

    Rename functions and update iteration constants

--- header/proyectoFinal.h
+++ header/proyectoFinal.h
@@ -4,16 +4,16 @@
 #include <string>
 
 #define PI 3.14159265358979323846
-#define ITERACIONES_TRIG 20  
-#define ITERACIONES_LOG 50
+#define REP_TRIG 20  
+#define REP_LOG 50
 
 bool resolverLaberinto(int arr[][7], int tamFila, int tamCol, int fila, int columna);
 
-double reducir_rango(double x);
-double mi_seno(double x);
-double mi_coseno(double x);
-double mi_ln(double x);
-double mi_tangente(double x);
+double reducirRango(double x);
+double miSeno(double x);
+double miCoseno(double x);
+double miLn(double x);
+double miTangente(double x);
 
 
 #endif
