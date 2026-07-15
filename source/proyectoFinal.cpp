3c66f68acd82eb6919b763805b1409e2db69ca9f
seven7739 <josuebalarezo0@gmail.com>
2026-07-15 00:21:51 -0500

    Add trigonometric and logarithmic functions

--- source/proyectoFinal.cpp
+++ source/proyectoFinal.cpp
@@ -88,3 +88,76 @@ int resolverLaberinto(int arr[][7], int tamFila, int tamCol, int fila, int colum
   	//se retorna si hubo éxito o no en la posición analizada
     return exito;
 }
+
+double reducirRango(double x) 
+{
+    while (x > PI) 
+	{
+        x -= 2.0 * PI;
+    }
+    while (x < -PI) 
+	{
+        x += 2.0 * PI;
+    }
+    return x;
+}
+
+double miSeno(double x) 
+{
+    x = reducirRango(x);
+    double termino = x;
+    double suma = termino;
+    
+    for (int n = 1; n < REP_TRIG; n++) 
+	{
+        termino = -termino * (x * x) / ((2 * n) * (2 * n + 1));
+        suma += termino;
+    }
+    return suma;
+}
+
+double miCoseno(double x)
+{
+    x = reducirRango(x);
+    double termino = 1.0;
+    double suma = termino;
+    
+    for (int n = 1; n < REP_TRIG; n++) {
+        termino = -termino * (x * x) / ((2 * n - 1) * (2 * n));
+        suma += termino;
+    }
+    return suma;
+}
+
+double miTangente(double x) 
+{
+    double cos_x = miCoseno(x);
+    if (cos_x > -0.000000001 && cos_x < 0.000000001) {
+        printf("Error: Tangente indefinida\n");
+        return 0.0; 
+    }
+    return miSeno(x) / cos_x;
+}
+
+double miLn(double x) 
+{
+    if (x <= 0.0) 
+	{
+        printf("Error: El logaritmo solo está definido para números positivos\n");
+        return 0.0;
+    }
+    
+    double y = (x - 1.0) / (x + 1.0);
+    double y2 = y * y;
+    double termino = y;
+    double suma = termino;
+    
+    for (int n = 1; n < REP_LOG; n++) 
+	{
+        termino *= y2;
+        suma += termino / (2 * n + 1);
+    }
+    
+    return 2.0 * suma;
+}
+
