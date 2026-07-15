026d2ad10c46d37784f8db25bab8e18c8eaaaaf5
seven7739 <josuebalarezo0@gmail.com>
2026-07-15 00:26:12 -0500

    Implement pruebaCaluladora for function testing
    Added a test function 'pruebaCaluladora' to validate trigonometric and logarithmic functions.

--- source/proyectoFinal.cpp
+++ source/proyectoFinal.cpp
@@ -161,3 +161,20 @@ double miLn(double x)
     return 2.0 * suma;
 }
 
+void pruebaCaluladora()
+{
+	double anguloRad = PI / 6.0; 
+    double e = 2.718281828;
+
+
+    printf("Seno(PI/6)     = %.6f (Esperado: 0.500000)\n", miSeno(anguloRad));
+    printf("Coseno(PI/6)   = %.6f (Esperado: 0.866025)\n", miCoseno(anguloRad));
+    printf("Tangente(PI/6) = %.6f (Esperado: 0.577350)\n\n", miTangente(anguloRad));
+
+    printf("ln(e)          = %.6f (Esperado: 1.000000)\n", miLn(e));
+    printf("ln(10)         = %.6f (Esperado: 2.302585)\n", miLn(10.0));
+    printf("ln(0.5)        = %.6f (Esperado: -0.693147)\n\n", miLn(0.5));
+
+    printf("Seno(13*PI/6)  = %.6f (Esperado: 0.500000)\n", miSeno(13.0 * PI / 6.0));
+}
+
